da25e5d27d49b5ac80df07c6e1bdea19088b0a0b
julie <perianmodely.julie@gmail.com>
2022-02-15 13:40:30 +0400

    add exo level 00

--- day_01/starter_day01_L00/all_variables.js
+++ day_01/starter_day01_L00/all_variables.js
@@ -1,1 +1,30 @@
 // *****Exercices: Data Types --> Level00 [6, 7, 8, 9, 10]*****
+//6
+// let bibi;
+// let choco;
+// let lolo;
+// let lulu;
+
+//7
+// let bibi="travail";
+//  let choco=8;
+//  let lolo=true;
+//  let lulu="casta";
+
+//8
+// let prenom="Julie";
+// let nom="PERI";
+// let nationalite="francaise";
+// let pays="France";
+// let age=20;
+
+//9
+// var prenom="julie",nom="PERI",nationalite="francaise",pays="France",age=20;
+// console.log(prenom,nom,nationalite,pays,age);
+
+
+//10
+// let myage=26;
+// let yourage=30;
+// console.log("j'ai "+myage+" ans.")
+// console.log("vous "+"avez "+yourage+" ans.")
--- day_01/starter_day01_L00/variable.js
+++ day_01/starter_day01_L00/variable.js
@@ -2,17 +2,23 @@
 
 
 // Declaring different variables of different data types
+let x = 20;  //type number
 
+let nomus = "julie"; //type string (chaine de caractères)
 
+let JulieForte = true;  //type boolean
+let JulieNulle = false; //type boolean  
 
+let stylux = null;   //valeur null (elle est vide la valeur)
+
+let yum // undefined (car elle n'a pas dde valeur)
 
 // Declaring variables with number values
 
-
-
-
+let aget = 20; 
+//console.log (typeof age)
 
 
 // Variables can also be declaring in one line separated by comma
-
+//let x = 20,stylux = null,nom = "julie";
 
